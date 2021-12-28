2e599a0f13226778562a2693e8b1576b75717532
Snoopymeoww <giahuy392001@gmail.com>
2021-12-28 09:38:56 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,15 +3,17 @@
 #include "stm32f10x_rcc.h"
 #include "stm32f10x_i2c.h"
 #include "delay.h"
+#include "at24c04.h"
 
 
 
 
 int main()
-{
+{	
+	delay_Config(72);
 	I2C1_init();
 	i2c_Write(0x50, 0x12, 0x13);
-	for(int i=10000; i>0;i--);
+	delay_ms(1000);
 	i2c_Read(0x50, 0x12);
 	while(1)
 	{
