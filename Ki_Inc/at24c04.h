7f99bd0e0654f1bdac55af19cbc18608b1df0410
Snoopymeoww <giahuy392001@gmail.com>
2021-12-28 09:36:24 +0300

    Update at24c04.h

--- Ki_Inc/at24c04.h
+++ Ki_Inc/at24c04.h
@@ -1,1 +1,9 @@
+#include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_i2c.h"
 
+void I2C1_init(void);
+void i2c_Write(uint32_t address, uint8_t address_Word, uint8_t data);
+void i2c_Read(uint32_t address, uint8_t address_Word);
+void i2c_Reset();
