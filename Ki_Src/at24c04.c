75b5e2a9f7d7a8c1137bf12650033fd97e873b81
Snoopymeoww <giahuy392001@gmail.com>
2021-12-28 09:35:22 +0300

    Update at24c04.c

--- Ki_Src/at24c04.c
+++ Ki_Src/at24c04.c
@@ -1,1 +1,81 @@
+#include "at24c04.h"
 
+
+
+void I2C1_init(void)
+{
+    I2C_InitTypeDef  I2C_InitStructure;
+    GPIO_InitTypeDef  GPIO_InitStructure;
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+
+    /* Configure I2C_EE pins: SCL and SDA */
+    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_6 | GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+    /* I2C configuration */
+    I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
+    I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
+    I2C_InitStructure.I2C_OwnAddress1 = 0x38;
+    I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
+    I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+    I2C_InitStructure.I2C_ClockSpeed = 100000; 
+
+    /* I2C Peripheral Enable */
+    I2C_Cmd(I2C1, ENABLE);
+    /* Apply I2C configuration after enabling it */
+    I2C_Init(I2C1, &I2C_InitStructure);
+}
+void i2c_Write(uint32_t address, uint8_t address_Word, uint8_t data)
+	{
+	I2C_AcknowledgeConfig(I2C1,ENABLE);
+	I2C_GenerateSTART(I2C1,ENABLE);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));//EV5 select master mode
+
+  I2C_Send7bitAddress(I2C1, address<<1, I2C_Direction_Transmitter);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+
+  I2C_SendData(I2C1,address_Word);//EV8_1 Choose data's address  to write into DR
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING));//EV8
+
+  I2C_SendData(I2C1,data);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING));//EV8
+	
+	I2C_SendData(I2C1,data+2);
+		while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));//EV8_2: End of sending data
+		
+
+  I2C_GenerateSTOP(I2C1,ENABLE);
+
+  //while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));//EV8_2
+	}
+void i2c_Read(uint32_t address, uint8_t address_Word)
+	{
+	I2C_AcknowledgeConfig(I2C1,ENABLE);
+  I2C_GenerateSTART(I2C1,ENABLE);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));//EV5 Select master mode
+
+  I2C_Send7bitAddress(I2C1, (address<<1), I2C_Direction_Transmitter);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));//EV6 Select Master transfer mode
+
+	I2C_SendData(I2C1, address_Word);//Choose data's address EV8_1
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING));
+	
+	I2C_GenerateSTART(I2C1,ENABLE);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));//EV5
+
+  I2C_Send7bitAddress(I2C1, (address<<1)|1, I2C_Direction_Receiver);
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));//EV6
+		  
+  while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED));
+	I2C_AcknowledgeConfig(I2C1, DISABLE);
+	I2C_GenerateSTOP(I2C1,ENABLE);
+  I2C_ReceiveData(I2C1);
+	}
+	void i2c_Reset()
+	{
+	
+	}
